af0f09e467bd6e009375adcb35e99fd1af4deda7
John Simpson <jms1@jms1.net>
2025-12-10 08:07:32 -0500

    Updated gh-pages initial-32-gbc298eb

--- toc.js
+++ toc.js
@@ -13,80 +13,83 @@ class MDBookSidebarScrollbox extends HTMLElement {
     <a href='https://github.com/kg4zow/mdbook-template/'>Github Repo</a>
 <!-- End content above ToC -->
 
-<ol class="chapter"><li class="chapter-item expanded affix "><a href="introduction.html">Introduction</a></li><li class="chapter-item expanded affix "><li class="spacer"></li><li class="chapter-item expanded "><a href="chapter_1.html">Chapter 1</a></li></ol>
+<ol class="chapter"><li class="chapter-item "><span class="chapter-link-wrapper"><a href="introduction.html">Introduction</a></span></li><li class="chapter-item "><li class="spacer"></li></li><li class="chapter-item "><span class="chapter-link-wrapper"><a href="chapter_1.html">Chapter 1</a></span></li></ol>
 <!-- Start version-commit content below ToC -->
     <hr/>
     <div class="part-title">Version</div>
     <div id="commit" class='version-commit-div'>
-        <span class='version-commit-hash'><tt>initial-28-ge5df9ea</tt></span><br/>
-        <span class='version-commit-time'><tt>2025-08-03 21:56:48 +0000</tt></span>
+        <span class='version-commit-time'><tt>initial-32-gbc298eb</tt></span>
+        <br/>
+        <span class='version-commit-hash'><tt>2025-12-10 13:07:01 +0000</tt></span>
     </div>
     <div class="part-title">Generated</div>
     <div id="generated" class='version-commit-div'>
-        <span class='version-commit-now'><tt>2025-08-03 21:57:17</tt></span>
+        <span class='version-commit-ver'>using <a href='https://rust-lang.github.io/mdBook/'><tt>mdbook v0.5.1</tt></a></span>
+        <br/>
+        <span class='version-commit-now'><tt>2025-12-10 13:07:32 +0000</tt></span>
     </div>
 <!-- End version-commit content below ToC -->
 
 
 `;
         // Set the current, active page, and reveal it if it's hidden
-        let current_page = document.location.href.toString().split("#")[0].split("?")[0];
-        if (current_page.endsWith("/")) {
-            current_page += "index.html";
+        let current_page = document.location.href.toString().split('#')[0].split('?')[0];
+        if (current_page.endsWith('/')) {
+            current_page += 'index.html';
         }
-        var links = Array.prototype.slice.call(this.querySelectorAll("a"));
-        var l = links.length;
-        for (var i = 0; i < l; ++i) {
-            var link = links[i];
-            var href = link.getAttribute("href");
-            if (href && !href.startsWith("#") && !/^(?:[a-z+]+:)?\/\//.test(href)) {
+        const links = Array.prototype.slice.call(this.querySelectorAll('a'));
+        const l = links.length;
+        for (let i = 0; i < l; ++i) {
+            const link = links[i];
+            const href = link.getAttribute('href');
+            if (href && !href.startsWith('#') && !/^(?:[a-z+]+:)?\/\//.test(href)) {
                 link.href = path_to_root + href;
             }
-            // The "index" page is supposed to alias the first chapter in the book.
-            if (link.href === current_page || (i === 0 && path_to_root === "" && current_page.endsWith("/index.html"))) {
-                link.classList.add("active");
-                var parent = link.parentElement;
-                if (parent && parent.classList.contains("chapter-item")) {
-                    parent.classList.add("expanded");
-                }
+            // The 'index' page is supposed to alias the first chapter in the book.
+            if (link.href === current_page
+                || i === 0
+                && path_to_root === ''
+                && current_page.endsWith('/index.html')) {
+                link.classList.add('active');
+                let parent = link.parentElement;
                 while (parent) {
-                    if (parent.tagName === "LI" && parent.previousElementSibling) {
-                        if (parent.previousElementSibling.classList.contains("chapter-item")) {
-                            parent.previousElementSibling.classList.add("expanded");
-                        }
+                    if (parent.tagName === 'LI' && parent.classList.contains('chapter-item')) {
+                        parent.classList.add('expanded');
                     }
                     parent = parent.parentElement;
                 }
             }
         }
         // Track and set sidebar scroll position
-        this.addEventListener('click', function(e) {
+        this.addEventListener('click', e => {
             if (e.target.tagName === 'A') {
                 sessionStorage.setItem('sidebar-scroll', this.scrollTop);
             }
         }, { passive: true });
-        var sidebarScrollTop = sessionStorage.getItem('sidebar-scroll');
+        const sidebarScrollTop = sessionStorage.getItem('sidebar-scroll');
         sessionStorage.removeItem('sidebar-scroll');
         if (sidebarScrollTop) {
             // preserve sidebar scroll position when navigating via links within sidebar
             this.scrollTop = sidebarScrollTop;
         } else {
-            // scroll sidebar to current active section when navigating via "next/previous chapter" buttons
-            var activeSection = document.querySelector('#sidebar .active');
+            // scroll sidebar to current active section when navigating via
+            // 'next/previous chapter' buttons
+            const activeSection = document.querySelector('#mdbook-sidebar .active');
             if (activeSection) {
                 activeSection.scrollIntoView({ block: 'center' });
             }
         }
         // Toggle buttons
-        var sidebarAnchorToggles = document.querySelectorAll('#sidebar a.toggle');
+        const sidebarAnchorToggles = document.querySelectorAll('.chapter-fold-toggle');
         function toggleSection(ev) {
-            ev.currentTarget.parentElement.classList.toggle('expanded');
+            ev.currentTarget.parentElement.parentElement.classList.toggle('expanded');
         }
-        Array.from(sidebarAnchorToggles).forEach(function (el) {
+        Array.from(sidebarAnchorToggles).forEach(el => {
             el.addEventListener('click', toggleSection);
         });
     }
 }
-window.customElements.define("mdbook-sidebar-scrollbox", MDBookSidebarScrollbox);
+window.customElements.define('mdbook-sidebar-scrollbox', MDBookSidebarScrollbox);
 
-// mdbook-fix-templates v0.2.0 2025-05-25 - mdbook v0.4.51
+
+// mdbook-fix-templates v0.3.0 2025-12-03 - mdbook v0.5.1
